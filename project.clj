dff505623974e9fe8831eca6682d17bdfebc760b
Jacob Emcken <jacob@emcken.dk>
2023-10-07 17:04:31 +0200

    Create first release for Clojars

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject dk.emcken/aws-simple-sign "1.0.0"
-  :description "A library to sign HTTP requests for AWS"
+  :description "A library to sign HTTP requests & generate presigned URL's for AWS"
+  :url "https://github.com/jacobemcken/aws-simple-sign"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
