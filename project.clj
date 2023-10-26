9605b7173321dbadb43e5ae3b4de2be7c325c3de
Jacob Emcken <jacob@emcken.dk>
2023-10-26 18:31:25 +0200

    Prepare for next release cycle

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject dk.emcken/aws-simple-sign "1.0.0"
+(defproject dk.emcken/aws-simple-sign "1.1.0-SNAPSHOT"
   :description "A library to sign HTTP requests & generate presigned URL's for AWS"
   :url "https://github.com/jacobemcken/aws-simple-sign"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [clojure-ini/clojure-ini "0.0.2"]])
