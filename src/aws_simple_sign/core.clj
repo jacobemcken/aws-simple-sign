jacobemcken/aws-simple-sign

commit 292190d604263cd0797ee17c460ea1c9067f631f

Fix wrong content SHA for 'UNSIGNED-PAYLOAD'

src/aws_simple_sign/core.clj
@@ -157,6 +157,14 @@
        (map (fn [[k v]] (str k ":" (some-> v str/trim) "\n")))
        (apply str)))
 
+(defn hash-input
+  "Takes input as either `String` or `InputStream`
+   to calculate and return a hash."
+  [payload]
+  (some-> (or payload "")
+          (hash-sha256)
+          (hex-encode-str)))
+
 (defn canonical-request-str
   "Generates a canonical request string as specified here:
    https://docs.aws.amazon.com/AmazonS3/latest/API/sig-v4-header-based-auth.html#canonical-request"
@@ -169,7 +177,7 @@
          (->query-str query-params) "\n"
          (->headers-str sorted-signed-headers) "\n"
          (str/join ";" (map key sorted-signed-headers)) "\n"
-         (or content-sha256 "UNSIGNED-PAYLOAD"))))
+         (or content-sha256 (hash-input nil)))))
 
 (defn signature
   "AWS specification: https://docs.aws.amazon.com/AmazonS3/latest/API/sig-v4-header-based-auth.html
@@ -186,14 +194,6 @@
                         :region region
                         :service service
                         :short-date (subs timestamp 0 8)})))
-
-(defn hash-input
-  "Takes input as either `String` or `InputStream`
-   to calculate and return a hash."
-  [payload]
-  (some-> (or payload "")
-          (hash-sha256)
-          (hex-encode-str)))
 
 (defn ^:no-doc get-query-params
   [params-str]
@@ -224,11 +224,10 @@
          scope (str (subs timestamp 0 8) "/" region "/" service "/aws4_request")
          content-sha256 (or payload-hash
                             (when (string? body) ; protect against consuming InputStreams which can only be consumed once.
-                              (hash-input body))
-                            "UNSIGNED-PAYLOAD")
+                              (hash-input body)))
          signed-headers (-> headers
                             (assoc "Host" host
-                                   "x-amz-content-sha256" content-sha256
+                                   "x-amz-content-sha256" (or content-sha256 "UNSIGNED-PAYLOAD")
                                    "x-amz-date" timestamp
                                    "x-amz-security-token" (:aws/session-token credentials)))
          signature-str (signature credentials

project.clj
@@ -1,4 +1,4 @@
-(defproject dk.emcken/aws-simple-sign "2.4.0-SNAPSHOT"
+(defproject dk.emcken/aws-simple-sign "2.3.1"
   :description "A library to sign HTTP requests & generate presigned URL's for AWS"
   :url "https://github.com/jacobemcken/aws-simple-sign"
   :license {:name "The MIT License"

commit 1c8fe33f0eaaf5dd944e0250e151bddc53f5e913

Fix presigned URLs content-sha256 value
Commit 292190d604 broke presigned URLs generation.

Presigned URLs always needs to use the constant string
"UNSIGNED-PAYLOAD" for their payload hash value (content-sha256).
Quoting from [1]:

    For more information about the signing process (details of
    creating a canonical request, string to sign, and signature
    calculations), see Signature Calculations for the Authorization
    Header: Transferring Payload in a Single Chunk (AWS Signature
    Version 4). The process is generally the same except that the
    creation of CanonicalRequest in a presigned URL differs as
    follows:

    * You don't include a payload hash in the Canonical Request,
      because when you create a presigned URL, you don't know the
      payload content because the URL is used to upload an arbitrary
      payload. Instead, you use a constant string UNSIGNED-PAYLOAD.

    [...]

[1] https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-query-string-auth.html

src/aws_simple_sign/core.clj
@@ -292,7 +292,8 @@
                                :scope scope
                                :method method
                                :query-params query-params
-                               :signed-headers {"host" host}})]
+                               :signed-headers {"host" host}
+                               :content-sha256 "UNSIGNED-PAYLOAD"})]
      (str (.getProtocol url-obj) "://" host (.getPath url-obj) "?"
           (->query-str query-params)
           "&X-Amz-Signature=" signature))))